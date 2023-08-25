0d32f9f7bd354e62fc96c08485148a4e440358e8
negitoro-gohan <44143453+negitoro-gohan@users.noreply.github.com>
2023-08-25 21:08:29 +0900

    Create connectString.ps1

---
+++ conf/connectString.ps1
@@ -0,0 +1,7 @@
+#カレントディレクトリをこのファイルが格納されているフォルダに変更
+class connectString {
+    [String] $Server = "TestServer"
+    [String] $Db = "TestDB"
+    [String] $User = "TestUser"
+    [String] $Password = "TestPassword"
+}
