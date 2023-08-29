decc822d8d0e04fda9a3edc20f06feb54497096e
negitoro-gohan <44143453+negitoro-gohan@users.noreply.github.com>
2023-08-30 00:11:19 +0900

    Delete conf directory

--- conf/connectString.ps1
+++
@@ -1,7 +0,0 @@
-#カレントディレクトリをこのファイルが格納されているフォルダに変更
-class connectString {
-    [String] $Server = "TestServer"
-    [String] $Db = "TestDB"
-    [String] $User = "TestUser"
-    [String] $Password = "TestPassword"
-}
